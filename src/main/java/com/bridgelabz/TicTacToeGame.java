156717496785c328dc6af8941ea641ecb8294c12
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-07-08 17:04:04 +0530

    Added:Ability to allow the player to choose a letter X or O

--- src/main/java/com/bridgelabz/TicTacToeGame.java
+++ src/main/java/com/bridgelabz/TicTacToeGame.java
@@ -1,19 +1,25 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
-    public static void main(String[] args)
-    {
-        System.out.println("Welcome to tic-tac-toe game");
-        char[] board= createBoard();
-    }
-    //uc1- create empty 10 size char array for board
-    private static char[] createBoard()
-    {
+    public static char[] createBoard() {
         char[] board = new char[10];
-        for(int i=1; i<board.length;i++)
-        {
-            board[i] = ' ';
+        for (int i = 0; i < board.length; i++) {
+            board[i] = ' '; //initializing empty values
         }
         return board;
+    }
+    public static char chooseUserLetter(Scanner userInput) {
+        System.out.println("Choose Your Letter X or O: ");
+        return userInput.next().toUpperCase().charAt(0);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Tic Tac Toe Game");
+        Scanner userInput = new Scanner(System.in);
+        char[] board = createBoard();
+        char userLetter = chooseUserLetter(userInput);
+        char computerLetter = (userLetter == 'X') ? 'O' : 'X';
     }
 }
